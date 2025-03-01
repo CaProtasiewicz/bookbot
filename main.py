d940b6411ea761c8aa550983b797b75e75134947
CaProtasiewicz <carol.ip@hotmail.com>
2025-03-01 17:19:19 -0300

    character count added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from stats import get_word_count
+from stats import character_count
 
 def main():
     filepath = "books/frankenstein.txt"
     print(f"{get_word_count(filepath)} words found in the document")
+    print(character_count(filepath))
     
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,20 @@ def get_word_count(filepath):
         for word in words_split:
             word_count += 1
     return word_count
+
+def character_count(filepath):
+    character_dict = {}
+    with open(filepath) as f:
+        book_text = f.read()
+        book_text = book_text.lower()
+        words_list = book_text.split()
+        words_split = "".join(words_list)
+        for character in words_split:
+           if character not in character_dict:
+                character_dict[character] = 1
+           else:
+               character_dict[character] += 1
+    return character_dict
+    
+    
+    
