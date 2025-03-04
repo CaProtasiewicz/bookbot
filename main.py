0d5f3d7deeb36390a3d94e6d2d74586a8d346700
CaProtasiewicz <carol.ip@hotmail.com>
2025-03-04 02:55:50 -0300

    character count with dictionary in stats and main update

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from stats import get_word_count
-from stats import character_count
+from stats import get_word_count, character_count, sort_dict
 
 def main():
     filepath = "books/frankenstein.txt"
-    print(f"{get_word_count(filepath)} words found in the document")
-    print(character_count(filepath))
+    
+    with open(filepath) as f:
+        contents = f.read()
+
+    word_count = get_word_count(contents)
+    char_count = character_count(contents)
+    sorted_chars = sort_dict(char_count)
+            
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char_data in sorted_chars:
+        if char_data["character"].isalpha():
+            print(f"{char_data["character"]}: {char_data["count"]}")
+    print("============= END ===============")
     
 main()
--- stats.py
+++ stats.py
@@ -1,25 +1,39 @@
-def get_word_count(filepath):
+def get_word_count(contents):
     word_count = 0
-    with open(filepath) as f:
-        book_text = f.read()
-        words_split = book_text.split()
-        for word in words_split:
-            word_count += 1
+    words_split = contents.split()
+    for word in words_split:
+        word_count += 1
     return word_count
 
-def character_count(filepath):
+def character_count(contents):
     character_dict = {}
-    with open(filepath) as f:
-        book_text = f.read()
-        book_text = book_text.lower()
-        words_list = book_text.split()
-        words_split = "".join(words_list)
-        for character in words_split:
-           if character not in character_dict:
-                character_dict[character] = 1
-           else:
-               character_dict[character] += 1
+    book_text = contents.lower()
+    for character in book_text:
+        if character not in character_dict:
+            character_dict[character] = 1
+        else:
+            character_dict[character] += 1
     return character_dict
+
+def sort_dict(dictionary):
+    char_list = []
     
+    # Loopa por cada caractere e sua contagem no dicionário
+    for character, count in dictionary.items():
+        # Cria um novo dicionário para esse caractere
+        char_dict = {
+            "character": character,
+            "count": count
+        }
+        # Adiciona esse dicionário para nossa lista
+        char_list.append(char_dict)
+        
+    # Organizar a lista baseado na contagem
+    # Função que define o valor para ser organizado com base    
+    def sort_on(char_dict):
+        return char_dict["count"]
     
+    # Organizar a lista em ordem decrescente (reverse = True)
+    char_list.sort(reverse=True, key=sort_on)
     
+    return char_list
